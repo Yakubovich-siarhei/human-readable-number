f38838744e25ecac3e5a80fcd2bf544d41e4c3cb
Yakubovich-siarhei <yakubovich.siarhei@mail.ru>
2023-02-07 14:20:35 +0300

    fix: 2.0

--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
 module.exports = function toReadable (number) {
-  const a = ['','one ','two ','three ','four ', 'five ','six ','seven ',
-              'eight ','nine ','ten ','eleven ','twelve ','thirteen ',
-              'fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
-  const b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-  if (number === 0) {
-    return 'zero';
+  let numToTwen = {
+    0: "",
+    1 : "one",
+    2: "two",
+    3 : "three",
+    4 : "four",
+    5 : "five",
+    6 : "six",
+    7 : "seven",
+    8 : "eight",
+    9 : "nine",
+    10 : "ten",
+    11 : "eleven",
+    12 : "twelve",
+    13 : "thirteen",
+    14 : "fourteen",
+    15 : "fifteen",
+    16 : "sixteen",
+    17 : "seventeen",
+    18 : "eighteen",
+    19 : "nineteen"
+  };
+
+  let numToHund = {
+    0: "",
+    1: "",
+    2 : "twenty",
+    3 : "thirty",
+    4 : "forty",
+    5 : "fifty",
+    6 : "sixty",
+    7 : "seventy",
+    8 : "eighty",
+    9 : "ninety"
+  };
+  let result = "";
+
+  if(number === 0) return "zero";
+
+  if(number >= 100) {
+    result += numToTwen[Math.floor(number / 100)] + ' hundred '
+  };
+
+  if(number % 100 < 20) {
+	result += numToTwen[number % 100];
+  } else {
+	result += numToHund[Math.floor((number % 100) / 10)];
+    if (number % 10) result += " " + numToTwen[number % 10];
   }
 
-  if (number < 10) {
-    return a[number];
-  }
-
+  return result.trim();
 }
